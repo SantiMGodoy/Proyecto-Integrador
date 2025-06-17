63cf97fb6965f56f9d2ee4112ca2bdd6aad6859b
Santiago Godoy <50716299+SantiMGodoy@users.noreply.github.com>
2025-06-17 18:06:58 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const DB_NAME = process.env.DB_NAME || "integrador";
+const DB_NAME = process.env.DB_NAME || "railway";
 const DB_USER = process.env.DB_USER || "root";
 const DB_PASSWORD = process.env.DB_PASSWORD || "root";
 const DB_HOST = process.env.DB_HOST || "mysql.railway.internal";
